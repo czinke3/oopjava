33326491e15552fc1fb1f7cfff9e2fddd85d81c9
czinke3 <czinke3@iit423.iit.uni-miskolc.hu>
2018-04-03 17:27:20 +0200

    c

--- 7gyak/src/myproductsRun/ProductsRun.java
+++ 7gyak/src/myproductsRun/ProductsRun.java
@@ -7,11 +7,28 @@ import myproducts.*;
 public class ProductsRun {
 
 	public static void main(String[] args) {
-		Product p1 = new Product("Product1", 100, 27);
+		Product p1 = new Product("Product1", 100, 50);
 		Bread b1 = new Bread("Bread1", 200, 27, 2);
 		
 		System.out.println("A p1 adatai: " + p1.toString());
 		System.out.println("A b1 adatai: " + b1.toString());
+		
+		System.out.println("");
+		
+		int priceCompare = p1.productCompare(b1);
+		if(priceCompare == 1)
+			System.out.println("A p1 ára a nagyobb.");
+		else if(priceCompare == -1)
+			System.out.println("A b1 ára a nagyobb");
+		else
+			System.out.println("A p1 és p2 ára ugyanannyi");
+		
+		System.out.println("");
+		
+		Bread b2 = new Bread("Bread2", 150, 24, 3);
+		Product product2 = b2;
+		System.out.println("A product2 adatai: " + product2.toString());
 	}
+	
 
 }
