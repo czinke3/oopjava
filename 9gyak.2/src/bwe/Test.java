e38b53e3d331c9b36d2d4a52a6de742cde397e0f
czinke3 <czinke3@iit423.iit.uni-miskolc.hu>
2018-04-17 17:39:50 +0200

    g

--- 9gyak.2/src/bwe/Test.java
+++ 9gyak.2/src/bwe/Test.java
@@ -1,12 +1,55 @@
 package bwe;
+import java.util.Scanner;
+
 import bwe.BookWithEnum.style;
 
 public class Test {
 
 	public static void main(String[] args) {
+		BookWithEnum[] bweArray;
+		Scanner sc = new Scanner(System.in);
 		
+		
+		System.out.println("Kérem az adatok számát: ");
+		int length = sc.nextInt();
+		
+		bweArray = new BookWithEnum[length];
+		
+		for(int i=0; i< bweArray.length; i++){
+		System.out.println("Kérem a címet: ");
+		String title = sc.next();
+		
+		System.out.println("Kérem a szerzõt: ");
+		String author = sc.next();
+		
+		System.out.println("Kérem a kiadási évet: ");
+		int dateOfissue = sc.nextInt();
+		
+		System.out.println("Kérem a árát: ");
+		int price = sc.nextInt();
+		
+		System.out.println("Kérem a stílust: ");
+		String styleString = sc.next();
+		style bookStyle = style.ROMANCE;
+		
+		if(styleString.contains("romance"))
+			bookStyle = style.ROMANCE;
+		
+		if(styleString.contains("scifi"))
+			bookStyle = style.SCIFI;
+		
+		if(styleString.contains("other"))
+			bookStyle = style.OTHER;
+		
+		if(styleString.contains("cook"))
+			bookStyle = style.COOK;
+		
+		bweArray[i] = new BookWithEnum(title, author, dateOfissue, price, bookStyle);
 	}
-	public static void bookDataWrite(Book[] bookArray) {
+		bookDataWrite(bweArray);
+		BookWithEnum[] bweArray2 = getBWE(bweArray, style.SCIFI);
+	}
+	public static void bookDataWrite(BookWithEnum[] bookArray) {
 		System.out.println("");
 		for(Book b : bookArray)
 			System.out.println(b.toString());
