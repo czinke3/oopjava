f0f47c136a46f44c910fa0f4ec6b55e6ce0845d8
czinke3 <czinke3@iit423.iit.uni-miskolc.hu>
2018-04-24 16:51:46 +0200

    a

--- 10/src/kivetel/Test.java
+++ 10/src/kivetel/Test.java
@@ -1,6 +1,9 @@
 package kivetel;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public  class Test {
 	public static void main(String[] args){
@@ -12,7 +15,7 @@ public  class Test {
 	}
 	public static void writeElements(int[] arrayIn){
 		for(int i=0; i<arrayIn.length; i++)
-			System.out.println("A tömb " + (i+1) + ". elemét: " + arrayIn[i]);
+			System.out.println("A tömb " + (i+1) + ". elemét: " + arrayIn);
 	}
 	public static void readNexIn(int[] arrayIn){
 		Scanner sc = new Scanner(System.in);
@@ -34,7 +37,23 @@ public  class Test {
 		}
 		sc.close();
 	}
-	
+	public static void readBRintValue(int[] arrayIn){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i=0; i<arrayIn.length; i++){			
+			System.out.println("Kérem a tömb " + i + ". elemét: ");
+			String line = "";
+			
+			try{
+				line = br.readLine();
+			}catch (IOException e){
+				e.printStackTrace();
+			}
+			
+			arrayIn[i] = Integer.valueOf(line).intValue();
+		}
+		
+	}
 	
 	
 }
